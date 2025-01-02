07904a58cd21682edd0d8e43d2c329d0df1a2864
tgg <qazplmtfgcv123@gmail.com>
2025-01-02 11:26:36 +0900

    test

--- src/main/java/com/example/calculator_lv1/Calculator.java
+++ src/main/java/com/example/calculator_lv1/Calculator.java
@@ -3,5 +3,6 @@ package com.example.calculator_lv1;
 public class Calculator {
     public static void main(String[] args){
         System.out.println("hello");
+        System.out.println("hi");
     }
 }
