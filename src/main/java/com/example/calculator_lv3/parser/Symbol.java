7e4a99caf533fb838dd40ca131c4a6d6bff4ce60
tgg <qazplmtfgcv123@gmail.com>
2025-01-06 16:36:25 +0900

    enum 클래스 Symbol 작성 완료

--- src/main/java/com/example/calculator_lv3/parser/Symbol.java
+++ src/main/java/com/example/calculator_lv3/parser/Symbol.java
@@ -1,5 +1,14 @@
 package com.example.calculator_lv3.parser;
 
 public enum Symbol {
+    ADD("+"),
+    SUBTRACT("-"),
+    MULTIPLY("*"),
+    DIVIDE("/");
 
+    private String symbol;
+
+    private Symbol(String symbol){
+        this.symbol = symbol;
+    }
 }
